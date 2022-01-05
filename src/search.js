71192261b2f8b13a3a1424a6e0ad89dcd1e9ce57
Samar Panda <er.samarpanda@gmail.com>
2022-01-05 08:45:22 +0530

    helping gnyan

--- src/search.js
+++ src/search.js
@@ -1,29 +1,24 @@
-function SearchPhotos(){
+function SearchPhotos() {
+  let clintId = 'yYSo1QyYlq66T7tUF2pNBBAT92KyMuWu_oCq_MSIjes';
+  let query = document.getElementById('search').value;
+  let url =
+    'https://api.unsplash.com/search/photos?query=' +
+    query +
+    '&client_id=Sa5iCe6pu_a1_m3EpC5LagCtFdbaeoXNRUndfjmlEpI&per_page=5';
 
-    let clintId="yYSo1QyYlq66T7tUF2pNBBAT92KyMuWu_oCq_MSIjes";
-    let query=document.getElementById("search").value;
-    let url="https://api.unsplash.com/search/photos?query="+query+"&client_id=Sa5iCe6pu_a1_m3EpC5LagCtFdbaeoXNRUndfjmlEpI&per_page=5"
-    
-    // make request to the api
-    fetch(url)
-    .then(function(data){
-            return data.json();
-        })
-    .then(function(data){
-        console.log(data);
-    
-        data.results.forEach(photo => {
-            $("#result").append(`
-            <img src="${photo.urls.thumb}"/>
-            `)
-            
-        }); 
-    
-    
-        });
-    }
-    
+  // make request to the api
+  fetch(url)
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (data) {
+      // console.log(data);
 
-    
-    
-    
+      const resultEl = document.querySelector('#result');
+      data.results.forEach((photo) => {
+        const imgEl = document.createElement('img');
+        imgEl.src = photo.urls.thumb;
+        resultEl.append(imgEl);
+      });
+    });
+}
